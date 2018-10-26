56edf9bdb93f3201edec3c8a74084337722abd16
stde11 <stde23@gmail.com>
2018-10-26 20:18:26 +0300

    started task-01 module-08

--- js-5+6/module-08/tasks/js/tasks.js
+++ js-5+6/module-08/tasks/js/tasks.js
@@ -5,11 +5,14 @@
 /*
   Напишите скрипт который реализует следующий функционал.
   
+  
   Есть кнопка с классом button, текст которой отображает 
   кол-во раз которое по ней кликнули, обновляется при каждом клике.
 */
 
 const button = document.querySelector(".button");
+
+//button.addEventListener("click", counterButtonClick);
 const counterButtonClick = () => {
     //let count = 0;
     function counter() {
