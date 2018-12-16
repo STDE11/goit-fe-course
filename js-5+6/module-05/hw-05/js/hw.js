8f1b372314b7d3dafe827522fbb782b358d12eb6
stde11 <stde23@gmail.com>
2018-12-16 21:37:14 +0200

    module - 05 js - HW is brought in changes: the vocabulary of pointer functions and chainlets of filters is Corrected.

--- js-5+6/module-05/hw-05/js/hw.js
+++ js-5+6/module-05/hw-05/js/hw.js
@@ -3,10 +3,6 @@
 //HW
 
 //Модуль 5 - Домашнее задание
-'use strict';
-
-//HW
-
 
 /*
   Используя массив (users) объектов пользователей, напишите функции которые с помощью 
@@ -114,9 +110,9 @@ const users = [
 
 
 //function getAllNames(arr) {
-const getAllNames = arr => {
-  return arr.map(obj => obj.name)
-};
+const getAllNames = arr => 
+  arr.map(obj => obj.name);
+
 
 console.log(getAllNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
@@ -125,9 +121,8 @@ console.log(getAllNames(users));
 /**
  * Получить массив объектов пользователей по цвету глаз (поле eyeColor)
  */
-const getUsersByEyeColor = (arr, eyeColor) => {
-  return arr.filter(obj => obj.eyeColor === eyeColor)
-};
+const getUsersByEyeColor = (arr, eyeColor) => 
+  arr.filter(obj => obj.eyeColor === eyeColor);
 
 console.log(getUsersByEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
@@ -135,12 +130,8 @@ console.log(getUsersByEyeColor(users, 'blue')); // [объект Moore Hensley, 
 /**
  * Получить массив имен пользователей по полу (поле gender)
  */
-const getUsersByGender = (arr, gender) => {
-  const objGenders = arr.filter(obj => obj.gender === gender);
-  return objGenders.map(objGender => objGender.name);
-
-  //return arr.filter(obj => obj.gender === gender).map(objGender => objGender.name);
-};
+const getUsersByGender = (arr, gender) => 
+  arr.filter(obj => obj.gender === gender).map(objGender => objGender.name);
 
 console.log(getUsersByGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
@@ -148,9 +139,8 @@ console.log(getUsersByGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazque
 /**
  * Получить массив только неактивных пользователей (поле isActive)
  */
-const getInactiveUsers = arr => {
-  return arr.filter(obj => !obj.isActive);
-};
+const getInactiveUsers = arr => 
+  arr.filter(obj => !obj.isActive);
 
 console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
@@ -158,9 +148,8 @@ console.log(getInactiveUsers(users)); // [объект Moore Hensley, объек
 /**
  * Получить пользоваля (не массив) по email (поле email, он уникальный)
  */
-const getUserByEmail = (arr, email) => {
-  return arr.find(obj => obj.email === email);
-};
+const getUserByEmail = (arr, email) => 
+  arr.find(obj => obj.email === email);
 
 console.log(getUserByEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
 console.log(getUserByEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
@@ -169,9 +158,8 @@ console.log(getUserByEmail(users, 'elmahead@omatom.com')); // {объект по
 /**
  * Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age)
  */
-const getUsersWithAge = (arr, min, max) => {
-  return arr.filter(obj => min < obj.age && obj.age < max);
-};
+const getUsersWithAge = (arr, min, max) => 
+  arr.filter(obj => min < obj.age && obj.age < max);
 
 console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 
@@ -182,10 +170,9 @@ console.log(getUsersWithAge(users, 30, 40));
 /**
  * Получить общую сумму баланса (поле balance) всех пользователей
  */
-const getTotalBalance = arr => {
+const getTotalBalance = arr => 
  
-  return arr.reduce((acc, obj) => acc + obj.balance, 0);
-};
+  arr.reduce((acc, obj) => acc + obj.balance, 0);
 
 console.log(getTotalBalance(users)); // 20916
 
@@ -193,109 +180,15 @@ console.log(getTotalBalance(users)); // 20916
 /**
  * Массив имен всех пользователей у которых есть друг с указанным именем
  */
-const getUsersByFriend = (arr, name) => {
+const getUsersByFriend = (arr, name) => 
   
-  const userWisFrends = arr.filter(obj => obj.friends.find(el => el === name));
-  
-  return userWisFrends.map(userWisFrend => userWisFrend.name);
-};
+ arr.filter(obj => obj.friends.find(el => el === name)).map(userWisFrend => userWisFrend.name);
+
 
 console.log(getUsersByFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersByFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
-// const users = [
-//   {
-//     id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
-//     name: 'Moore Hensley',
-//     email: 'moorehensley@indexia.com',
-//     eyeColor: 'blue',
-//     phone: '+1 (848) 556-2344',
-//     friends: ['Sharron Pace'],
-//     isActive: false,
-//     balance: 2811,
-//     skills: ['ipsum', 'lorem'],
-//     gender: 'male',
-//     age: 37,
-//   },
-//   {
-//     id: '7a3cbd18-57a1-4534-8e12-1caad921bda1',
-//     name: 'Sharlene Bush',
-//     email: 'sharlenebush@tubesys.com',
-//     eyeColor: 'blue',
-//     phone: '+1 (855) 582-2464',
-//     friends: ['Briana Decker', 'Sharron Pace'],
-//     isActive: true,
-//     balance: 3821,
-//     skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
-//     gender: 'female',
-//     age: 34,
-//   },
-//   {
-//     id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
-//     name: 'Ross Vazquez',
-//     email: 'rossvazquez@xinware.com',
-//     eyeColor: 'green',
-//     phone: '+1 (814) 593-3825',
-//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-//     isActive: false,
-//     balance: 3793,
-//     skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
-//     gender: 'male',
-//     age: 24,
-//   },
-//   {
-//     id: '249b6175-5c30-44c6-b154-f120923736f5',
-//     name: 'Elma Head',
-//     email: 'elmahead@omatom.com',
-//     eyeColor: 'green',
-//     phone: '+1 (909) 547-2687',
-//     friends: ['Goldie Gentry', 'Aisha Tran'],
-//     isActive: true,
-//     balance: 2278,
-//     skills: ['adipisicing', 'irure', 'velit'],
-//     gender: 'female',
-//     age: 21,
-//   },
-//   {
-//     id: '334f8cb3-eb04-45e6-abf4-4935dd439b70',
-//     name: 'Carey Barr',
-//     email: 'careybarr@nurali.com',
-//     eyeColor: 'blue',
-//     phone: '+1 (956) 512-2693',
-//     friends: ['Jordan Sampson', 'Eddie Strong'],
-//     isActive: true,
-//     balance: 3951,
-//     skills: ['ex', 'culpa', 'nostrud'],
-//     gender: 'male',
-//     age: 27,
-//   },
-//   {
-//     guid: '150b00fb-dd82-427d-9faf-2879ea87c695',
-//     name: 'Blackburn Dotson',
-//     email: 'blackburndotson@furnigeer.com',
-//     eyeColor: 'brown',
-//     phone: '+1 (876) 411-2433',
-//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
-//     isActive: false,
-//     balance: 1498,
-//     skills: ['non', 'amet', 'ipsum'],
-//     gender: 'male',
-//     age: 38,
-//   },
-//   {
-//     id: 'e1bf46ab-7168-491e-925e-f01e21394812',
-//     name: 'Sheree Anthony',
-//     email: 'shereeanthony@kog.com',
-//     eyeColor: 'brown',
-//     phone: '+1 (979) 504-2554',
-//     friends: ['Goldie Gentry', 'Briana Decker'],
-//     isActive: true,
-//     balance: 2764,
-//     skills: ['lorem', 'veniam', 'culpa'],
-//     gender: 'female',
-//     age: 39,
-//   },
-// ];
+
 
 
 // /*
